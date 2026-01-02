ca39de54190a1c541c2919a361dd1a5059ded66a
Hp <rs739025@gmail.com>
2026-01-02 23:17:09 +0530

    add view booking routes

--- backend/src/routes/booking.route.ts
+++ backend/src/routes/booking.route.ts
@@ -3,6 +3,7 @@ import { createBookingController } from "../controllers/booking.controller";
 import { authMiddleware } from "../middleware/auth.middleware";
 import { rbac } from "../rbac/permission/rbac.middleware";
 import { PERMISSIONS } from "../rbac/permission/permission";
+import { getMyBookingsController } from "../controllers/user_getbooking.controller";
 
 const bookingRouter = Router();
 
@@ -11,5 +12,10 @@ bookingRouter.post("/bookings",
     rbac(PERMISSIONS.BOOKING_CREATE),
     createBookingController
 );
-
+bookingRouter.get(
+    "/my/bookings",
+    authMiddleware,
+    rbac([PERMISSIONS.BOOKING_VIEW_OWN]),
+    getMyBookingsController
+  );
 export default bookingRouter;
